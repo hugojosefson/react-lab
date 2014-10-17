861418a58bf451dd8f5fcbec2bf9dcd151b7abfd
Joacim Löwgren <joacim.lowgren@gmail.com>
2014-10-17 06:28:48 +0200

    Add more elements

--- scripts/scrumtastic.js
+++ scripts/scrumtastic.js
@@ -1,6 +1,64 @@
 /** @jsx React.DOM */
 
+/* Components
+ * ======================================================================== */
+
+/* Wrapper */
+
 React.renderComponent(
-  <div className="page-wrap"></div>,
-  document.getElementById('body')
+  <div className="page-wrap" />,
+  document.querySelector('body')
+);
+
+
+/* Header */
+
+var Header = React.createClass({
+  render: function () {
+    return (
+      <header className="header" role="header">
+        <h1>{this.props.title}</h1>
+      </header>
+    );
+  }
+});
+
+
+/* Main */
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      <main className="main" role="main">
+        <ScrumBoard />
+      </main>
+    );
+  }
+});
+
+
+/* Scrum Board */
+
+var ScrumBoard = React.createClass({
+  render: function () {
+    var columns; // TODO
+    return (
+      <div className="scrum-board">
+      </div>
+    );
+  }
+});
+
+
+/* Render
+ * ======================================================================== */
+
+/* Start rendering chain */
+
+React.renderComponent(
+  <div className="page-wrap">
+    <Header title="SCRUMtastic" />
+    <Main />
+  </div>,
+  document.querySelector('body')
 );
